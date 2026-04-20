74e773f17047ad83ee452d626bd494a848ec8272
Athan Tsokolas <athan_tsokolas@yahoo.com>
2026-04-19 19:33:45 -0500

    Add missing HighlightsIntoConcepts page

---
+++ note-taker-ui/src/pages/HighlightsIntoConcepts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GuideArticlePage from '../components/seo/GuideArticlePage';
+
+const HighlightsIntoConcepts = () => (
+  <GuideArticlePage slug="highlights-into-concepts" />
+);
+
+export default HighlightsIntoConcepts;
