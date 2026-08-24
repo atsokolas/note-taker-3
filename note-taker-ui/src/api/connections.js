1d12e2b66c9b19f2ccb0abaf5884953b3c0b6cc0
Athan Tsokolas <athan_tsokolas@yahoo.com>
2026-08-24 15:56:32 -0500

    Coalesce exact connection graph reads

--- note-taker-ui/src/api/connections.js
+++ note-taker-ui/src/api/connections.js
@@ -1,23 +1,50 @@
 import api from '../api';
 import { getAuthHeaders } from '../hooks/useAuthHeaders';
+import { getStoredAuthScope } from '../system/authScopedSnapshotCache';
+import { clearCachedPrefix, fetchWithCache } from '../utils/cache';
+
+const CONNECTION_CACHE_TTL_MS = 30_000;
+const CONNECTION_CACHE_PREFIX = 'connections:item:';
+let connectionCacheGeneration = 0;
+
+const clearConnectionCache = () => {
+  connectionCacheGeneration += 1;
+  clearCachedPrefix(CONNECTION_CACHE_PREFIX);
+};
 
 export const createConnection = async (payload) => {
   const res = await api.post('/api/connections', payload, getAuthHeaders());
+  clearConnectionCache();
   return res.data;
 };
 
-export const getConnectionsForItem = async ({ itemType, itemId, scopeType = '', scopeId = '' }) => {
+export const getConnectionsForItem = async ({
+  itemType,
+  itemId,
+  scopeType = '',
+  scopeId = '',
+  force = false
+}) => {
   const params = new URLSearchParams();
   params.set('itemType', itemType);
   params.set('itemId', itemId);
   if (scopeType) params.set('scopeType', scopeType);
   if (scopeId) params.set('scopeId', scopeId);
-  const res = await api.get(`/api/connections?${params.toString()}`, getAuthHeaders());
-  return res.data || { outgoing: [], incoming: [] };
+  const path = `/api/connections?${params.toString()}`;
+  const cacheKey = `${CONNECTION_CACHE_PREFIX}${connectionCacheGeneration}:${getStoredAuthScope()}:${path}`;
+  return fetchWithCache(
+    cacheKey,
+    async () => {
+      const res = await api.get(path, getAuthHeaders());
+      return res.data || { outgoing: [], incoming: [] };
+    },
+    { ttlMs: CONNECTION_CACHE_TTL_MS, force: Boolean(force) }
+  );
 };
 
 export const deleteConnection = async (id) => {
   const res = await api.delete(`/api/connections/${id}`, getAuthHeaders());
+  clearConnectionCache();
   return res.data;
 };
 
